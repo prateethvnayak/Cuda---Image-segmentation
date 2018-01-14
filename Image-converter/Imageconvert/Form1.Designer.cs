e73be8fa9f14ee20cc94f33ad884ae87d6abbcfb
prateethvnayak <34733770+prateethvnayak@users.noreply.github.com>
2018-01-14 14:08:00 -0600

    Add files via upload

---
+++ Image-converter/Imageconvert/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ImageConverter
+{
+    partial class imageViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputPictureBox = new System.Windows.Forms.PictureBox();
+            this.outputPictureBox = new System.Windows.Forms.PictureBox();
+            this.openImageButton = new System.Windows.Forms.Button();
+            this.generateeOutputText = new System.Windows.Forms.Button();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.getImageButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.inputPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputPictureBox
+            // 
+            this.inputPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.inputPictureBox.Location = new System.Drawing.Point(49, 49);
+            this.inputPictureBox.Name = "inputPictureBox";
+            this.inputPictureBox.Size = new System.Drawing.Size(574, 417);
+            this.inputPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.inputPictureBox.TabIndex = 0;
+            this.inputPictureBox.TabStop = false;
+            // 
+            // outputPictureBox
+            // 
+            this.outputPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputPictureBox.Location = new System.Drawing.Point(753, 49);
+            this.outputPictureBox.Name = "outputPictureBox";
+            this.outputPictureBox.Size = new System.Drawing.Size(574, 417);
+            this.outputPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.outputPictureBox.TabIndex = 1;
+            this.outputPictureBox.TabStop = false;
+            // 
+            // openImageButton
+            // 
+            this.openImageButton.Location = new System.Drawing.Point(49, 620);
+            this.openImageButton.Name = "openImageButton";
+            this.openImageButton.Size = new System.Drawing.Size(106, 40);
+            this.openImageButton.TabIndex = 2;
+            this.openImageButton.Text = "Open Image";
+            this.openImageButton.UseVisualStyleBackColor = true;
+            this.openImageButton.Click += new System.EventHandler(this.openImageButton_Click);
+            // 
+            // generateeOutputText
+            // 
+            this.generateeOutputText.Location = new System.Drawing.Point(426, 620);
+            this.generateeOutputText.Name = "generateeOutputText";
+            this.generateeOutputText.Size = new System.Drawing.Size(180, 40);
+            this.generateeOutputText.TabIndex = 3;
+            this.generateeOutputText.Text = "Generate!!";
+            this.generateeOutputText.UseVisualStyleBackColor = true;
+            this.generateeOutputText.Click += new System.EventHandler(this.generateeOutputText_Click);
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(753, 620);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(92, 40);
+            this.openFileButton.TabIndex = 4;
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // getImageButton
+            // 
+            this.getImageButton.Location = new System.Drawing.Point(1204, 620);
+            this.getImageButton.Name = "getImageButton";
+            this.getImageButton.Size = new System.Drawing.Size(123, 40);
+            this.getImageButton.TabIndex = 5;
+            this.getImageButton.Text = "Get Image!!";
+            this.getImageButton.UseVisualStyleBackColor = true;
+            this.getImageButton.Click += new System.EventHandler(this.getImageButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Open Original Image First!!";
+            // 
+            // imageViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1411, 700);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.getImageButton);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.generateeOutputText);
+            this.Controls.Add(this.openImageButton);
+            this.Controls.Add(this.outputPictureBox);
+            this.Controls.Add(this.inputPictureBox);
+            this.Name = "imageViewForm";
+            this.Text = "Image View";
+            ((System.ComponentModel.ISupportInitialize)(this.inputPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox inputPictureBox;
+        private System.Windows.Forms.PictureBox outputPictureBox;
+        private System.Windows.Forms.Button openImageButton;
+        private System.Windows.Forms.Button generateeOutputText;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button getImageButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Image-converter/Imageconvert/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageConverter
+{
+    public partial class imageViewForm : Form
+    {
+        public imageViewForm()
+        {
+            InitializeComponent();
+            inputImage = null;
+        }
+        UInt32[] data;
+        public Bitmap inputImage;
+        private void ConvertRGB2Grey(Bitmap inputImage)
+        {
+            try
+            {
+                var dlg = new FolderBrowserDialog();
+                dlg.Description = "Set the Path for the data of the given file";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter sr = new StreamWriter(dlg.SelectedPath + "\\data.txt", false))
+                    {
+                        for (int i = 0; i < inputImage.Width; i++)
+                        {
+                            for (int j = 0; j < inputImage.Height; j++)
+                            {
+                                Color c = inputImage.GetPixel(i, j);
+                                int grey = (int)((c.R * 0.3) + (c.G * 0.59) + (c.B * 0.11));
+                                sr.Write(grey.ToString() + "\n");
+                            }
+                        }
+                    }
+                    using (StreamWriter sr = new StreamWriter(dlg.SelectedPath + "\\size.txt", false))
+                    {
+                        sr.Write(inputImage.Width.ToString() + " " + inputImage.Height.ToString());
+                    }
+                }
+            }
+            catch (IOException ie)
+            {
+                MessageBox.Show(ie.Message, "Error!!");
+            }
+            
+                
+            
+        }
+        private void openImageButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Open Image";
+            if(dlg.ShowDialog()==DialogResult.OK)
+            {
+                inputImage = new Bitmap(dlg.FileName);
+                Bitmap displayImage = GenerateDisplayImage(inputImage);
+                inputPictureBox.Image = displayImage;
+            }
+            dlg.Dispose();
+        }
+
+        private void generateeOutputText_Click(object sender, EventArgs e)
+        {
+            if (inputImage == null)
+            {
+                MessageBox.Show("Error", "Input Original Image First!!!");
+                return;
+            }
+            ConvertRGB2Grey(inputImage);
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            if (inputImage == null)
+            {
+                MessageBox.Show("Error", "Input Original Image First!!!");
+                return;
+            }
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Open File";
+            if(inputImage==null)
+            {
+                MessageBox.Show("Error", "Input Original Image First!!!");
+                return;
+            }
+            data = new UInt32[inputImage.Width * inputImage.Height];
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(dlg.FileName);
+                try
+                {
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                for (int i = 0; i < inputImage.Width; i++)
+                {
+                    for (int j = 0; j < inputImage.Height; j++)
+                    {
+                        string character = sr.ReadLine();
+                        
+                        data[i * inputImage.Height + j] = Convert.ToUInt32(character);
+                    }
+                }
+            }
+            dlg.Dispose();
+        }
+
+        private void getImageButton_Click(object sender, EventArgs e)
+        {
+            if (inputImage == null)
+            {
+                MessageBox.Show("Error", "Input Original Image First!!!");
+                return;
+            }
+            Bitmap outputImageGray = new Bitmap(inputImage);
+            for (int i = 0; i < inputImage.Width; i++)
+            {
+                for (int j = 0; j < inputImage.Height; j++)
+                {
+                    int datatmp = (int)data[i * inputImage.Height + j];
+                    outputImageGray.SetPixel(i, j, Color.FromArgb(datatmp, datatmp, datatmp));
+                }
+            }
+            outputPictureBox.Image = GenerateDisplayImage(outputImageGray);
+        }
+
+        private Bitmap GenerateDisplayImage(Bitmap inputImage)
+        {
+            float ratio = inputImage.Width / (float)inputImage.Height;
+            int newWidth, newHeight;
+            if (ratio > 1.5)
+            {
+                newWidth = 600;
+                newHeight = (int)(newWidth / ratio);
+            }
+            else
+            {
+                newHeight = 400;
+                newWidth = (int)(newHeight * ratio);
+            }
+            Bitmap displayImage = new Bitmap(inputImage, new Size(newWidth, newHeight));
+            return displayImage;
+        }
+    }
+}
